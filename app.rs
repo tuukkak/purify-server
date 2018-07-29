b4042592528c2722c569bbe2e762eb75ee754c88
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2018-07-29 17:20:36 +0300

    Packet struct and packet unpacker

--- app.rs
+++ app.rs
@@ -2,6 +2,20 @@ use std::thread;
 use std::time::{SystemTime, Duration};
 use std::net::UdpSocket;
 
+struct Player {
+    id: u8,
+    name: String,
+}
+
+#[derive(Debug, Default)]
+struct Packet {
+    packet_type: u8,
+    player_id: u8,
+    input_x: i8,
+    input_z: i8,
+    rotation: f32,
+}
+
 fn main() {
     println!("Server started...");
 
@@ -20,10 +34,13 @@ fn main() {
 
         // Handle all received datagrams
         'udp: loop {
-            let mut buf = [0; 1500];
+            let mut buf: [u8; 1500] = [0; 1500];
             match socket.recv_from(&mut buf) {
                 Ok((number_of_bytes, src_addr)) => {
-                    println!("Player joined!");
+                    println!("Datagram received!");
+                    let packet: Packet = unpack_packet(buf, number_of_bytes);
+                    println!("Packet length: {}", number_of_bytes);
+                    println!("Player id: {}", packet.player_id);
                 },
                 Err(_) => {
                     break 'udp;
@@ -34,4 +51,30 @@ fn main() {
         // Sleep before next round
         thread::sleep(Duration::from_millis(500 - loop_time.elapsed().unwrap().subsec_nanos() as u64 / 1000000));
     }
+}
+
+fn unpack_packet(buf: [u8; 1500], number_of_bytes: usize) -> Packet {
+    let mut packet = Packet {
+        packet_type: buf[0],
+        player_id: buf[1],
+        ..Default::default()
+    };
+
+    match packet.packet_type {
+        0 => {
+            println!("Login packet!");
+        },
+        1 => {
+            println!("Movement packet!");
+            packet.input_x = buf[2];
+            packet.input_z = buf[3];
+        },
+        2 => {
+            println!("Rotation packet!")
+        },
+        3 => println!("Spell packet!"),
+        _ => println!("Unknown packet!"),
+    };
+
+    packet
 }
